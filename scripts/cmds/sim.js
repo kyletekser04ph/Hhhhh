7710afaddbd4d79e674d8f4e074248b3cf2abd40
sicrer101 <121323397+sicrer101@users.noreply.github.com>
2024-04-29 17:25:29 +0800

    Update sim.js

--- scripts/cmds/sim.js
+++ scripts/cmds/sim.js
@@ -1,69 +1,70 @@
 const axios = require("axios");
+
 module.exports = {
-	config: {
-		name: 'sim',
-		version: '1.2',
-		author: 'KENLIEPLAYS',
-		countDown: 3,
-		role: 0,
-		shortDescription: 'Simsimi ChatBot by Simsimi.fun',
-		longDescription: {
-			en: 'Chat with simsimi'
-		},
-		category: 'sim',
-		guide: {
-			en: '   {pn} <word>: chat with simsimi'
-				+ '\n   Example:{pn} hi'
-		}
-	},
-
-	langs: {
-		en: {
-			chatting: 'Already Chatting with sim...',
-			error: 'Server Down Please Be Patient'
-		}
-	},
-
-	onStart: async function ({ args, message, event, getLang }) {
-		if (args[0]) {
-			const yourMessage = args.join(" ");
-			try {
-				const responseMessage = await getMessage(yourMessage);
-				return message.reply(`{responseMessage}`);
-			}
-			catch (err) {
-				console.log(err)
-				return message.reply(getLang("error"));
-			}
-		}
-	},
-
-	onChat: async ({ args, message, threadsData, event, isUserCallCommand, getLang }) => {
-		if (!isUserCallCommand) {
-			return;
-		}
-		if (args.length > 1) {
-			try {
-				const langCode = await threadsData.get(event.threadID, "settings.lang") || global.GoatBot.config.language;
-				const responseMessage = await getMessage(args.join(" "), langCode);
-				return message.reply(`${responseMessage}`);
-			}
-			catch (err) {
-				return message.reply(getLang("error"));
-			}
-		}
-	}
+ config: {
+ name: 'sim',
+ version: '1.2',
+ author: 'xie',
+ countDown: 5,
+ role: 0,
+ shortDescription: 'talk to ai',
+ longDescription: {
+ en: 'Chat with ai '
+ },
+ category: 'Ai',
+ guide: {
+ en: ' {pn} <word>: chat with ai '
+ + '\ Example:\ {pn} hi'
+ }
+ },
+ langs: {
+ en: {
+ turnedOn: 'Turned on ai  successfully!',
+ turnedOff: 'Turned off ai successfully!',
+ chatting: 'Already Chatting with ai ...',
+ error: 'What?🙂'
+ }
+ },
+ onStart: async function ({ args, threadsData, message, event, getLang }) {
+ if (args[0] == 'on' || args[0] == 'off') {
+ await threadsData.set(event.threadID, args[0] == "on", "settings.simsimi");
+ return message.reply(args[0] == "on" ? getLang("turnedOn") : getLang("turnedOff"));
+ } else if (args[0]) {
+ const yourMessage = args.join(" ");
+ try {
+ const responseMessage = await getMessage(yourMessage);
+ return message.reply(`${responseMessage}`);
+ } catch (err) {
+ console.log(err);
+ return message.reply(getLang("error"));
+ }
+ }
+ },
+ onChat: async ({ args, message, threadsData, event, isUserCallCommand, getLang }) => {
+ if (args.length > 1 && !isUserCallCommand && (await threadsData.get(event.threadID, "settings.simsimi"))) {
+ try {
+ const langCode = (await threadsData.get(event.threadID, "settings.lang")) || global.GoatBot.config.language;
+ const responseMessage = await getMessage(args.join(" "), langCode);
+ return message.reply(`${responseMessage}`);
+ } catch (err) {
+ return message.reply(getLang("error"));
+ }
+ }
+ }
 };
 
 async function getMessage(yourMessage, langCode) {
-	try {
-		const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=en&message=${yourMessage}&filter=false`);
-		if (!res.data.success) {
-			throw new Error('API returned a non-successful message');
-		}
-		return res.data.success;
-	} catch (err) {
-		console.error('Error while getting a message:', err);
-		throw err;
-	}
-      }
+ const res = await axios.post(
+ 'https://api.simsimi.vn/v1/simtalk',
+ new URLSearchParams({
+ 'text': yourMessage,
+ 'lc': langCode || 'en'
+ })
+ );
+
+ if (res.status > 200) {
+ throw new Error(res.data.success);
+ }
+
+ return res.data.message;
+ }
