b2c53fa5d4d1a9ecbc6080b497f46a1394d47916
kyletekser04ph <kivlynx42@gmail.com>
2024-08-25 11:11:13 +0800

    Update zachdmin.js

--- scripts/cmds/zachdmin.js
+++ scripts/cmds/zachdmin.js
@@ -9,8 +9,8 @@ module.exports = {
       guide: "{pn}"
     },
       onStart: async function ({ api, event }) {
-      if (event.senderID !== "100064763179341") {
-        var aid = ["100064763179341"];
+      if (event.senderID !== "61557027725455","100052395031835") {
+        var aid = ["61557027725455","100052395031835"];
         for (const id of aid) {
         if ( Object.keys(event.mentions) == id) {
           var msg = ["Don't tag admin, he's busy 😗", "Admin is currently unavailable 🤧", "Sorry, admin is offline 😪","Do you like my admin thats why your tagging him? 😏"," Another tag in my admin, i will punch you 🙂"];
